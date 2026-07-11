7817147a516c9d2467c9ad8504a4d5ee62ba5c9c
Rishabh Tater <rishabhtater142@gmail.com>
2026-07-11 16:01:54 +0530

    Context API Retest

---
+++ ContextAPI Retest/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ToggleBtn from './components/ToggleBtn'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightMode = () => {
+    setThemeMode("light")
+  }
+
+  const darkMode = () => {
+    setThemeMode("dark")
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
+
+  return (
+    <ThemeProvider value={{ themeMode, lightMode, darkMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ToggleBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+              <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
